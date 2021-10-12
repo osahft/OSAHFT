6247afcb2d2183c514ce6f9b8a16d21cf042d360
dnzxy <d.c.cengiz@googlemail.com>
2021-10-12 20:02:58 +0200

    Added some unused imports to test sonar; commit for testing - WIP

--- packages/app/src/app/app.module.ts
+++ packages/app/src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TagInputModule} from 'ngx-chips';
+import {AngularEditorModule} from '@kolkov/angular-editor';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
