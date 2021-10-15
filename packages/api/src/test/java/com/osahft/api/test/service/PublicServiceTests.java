8667b0abef5708309efe648c184589fc3aa85886
bastian <bastian.beier@st.oth-regensburg.de>
2021-10-15 21:13:31 +0200

    Fix unit tests failing because of missing test.properties.

--- packages/api/src/test/java/com/osahft/api/test/service/PublicServiceTests.java
+++ packages/api/src/test/java/com/osahft/api/test/service/PublicServiceTests.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static com.osahft.api.internal.assertion.Assertions.assertThat;
 
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties")
 public class PublicServiceTests {
     @Autowired
     private BuildProperties buildProperties;
